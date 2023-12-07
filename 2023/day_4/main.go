2343d0c4cd1d2f05ae5be4a950f9b70c27b8e0a7
Ben Maddux <bmaddux@umich.edu>
2023-12-06 20:32:47 -0600

    AoC: Day 4 complete

--- 2023/day_4/main.go
+++ 2023/day_4/main.go
@@ -57,6 +57,32 @@ func parse_card(line string) int {
 	return 0
 }
 
+func parse_cards2(line string) int {
+	numbers := strings.Split(line, ":")[1]
+
+	winning_numbers_str := strings.TrimSpace(strings.Split(numbers, "|")[0])
+	our_numbers_str := strings.TrimSpace(strings.Split(numbers, "|")[1])
+
+	winning_numbers := strings.Split(winning_numbers_str, " ")
+	for i := 0; i < len(winning_numbers); i += 1 {
+		winning_numbers[i] = strings.TrimSpace(winning_numbers[i])
+	}
+	our_numbers := strings.Split(our_numbers_str, " ")
+	for i := 0; i < len(our_numbers); i += 1 {
+		our_numbers[i] = strings.TrimSpace(our_numbers[i])
+	}
+	matches := 0
+	for i := 0; i < len(winning_numbers); i += 1 {
+		if len(winning_numbers[i]) == 0 {
+			continue
+		}
+		if contains(our_numbers, winning_numbers[i]) {
+			matches += 1
+		}
+	}
+	return matches
+}
+
 func main() {
 	lines := utils.Open_file("puzzle_input.txt")
 
@@ -65,5 +91,24 @@ func main() {
 		val := parse_card(line)
 		sum += val
 	}
-	fmt.Println(sum)
+	fmt.Println("Part 1: ", sum)
+
+	var card_counts = make([]int, len(lines))
+	for i := 0; i < len(card_counts); i++ {
+		card_counts[i] = 1
+	}
+
+	for i := 0; i < len(card_counts); i++ {
+		var num_won = parse_cards2(lines[i])
+		for j := 1; j <= num_won; j++ {
+			card_counts[i+j] += card_counts[i]
+		}
+	}
+
+	sum = 0
+	for i := 0; i < len(card_counts); i++ {
+		sum += card_counts[i]
+	}
+
+	fmt.Println("Part 2: ", sum)
 }
