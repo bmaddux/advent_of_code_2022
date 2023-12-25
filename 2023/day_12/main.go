42086230dc59b5e32683770d72e1b9c8063f24a6
Ben Maddux <bmaddux@umich.edu>
2023-12-25 13:16:29 -0600

    AoC: Day 12 part 2 complete

--- 2023/day_12/main.go
+++ 2023/day_12/main.go
@@ -3,73 +3,61 @@ package main
 import (
 	"2023/utils"
 	"fmt"
-	"slices"
 	"strconv"
 	"strings"
 )
 
-func validate_record(record []rune, counts []int) bool {
-	live_counts := make([]int, 0, len(counts))
-	spring_len := 0
-	counts_index := 0
-	for i := 0; i < len(record); i++ {
-		switch record[i] {
-		case '#':
-			{
-				// Adding an extra spring
-				if spring_len == 0 && counts_index >= len(counts) {
-					return false
-				}
-				spring_len++
-				// Adding to many springs to the current block
-				if spring_len > counts[counts_index] {
-					return false
-				}
-			}
-		case '?':
-			{
-				return true
-			}
-		case '.':
-			{
-				if spring_len > 0 {
-					live_counts = append(live_counts, spring_len)
-					counts_index++
-					spring_len = 0
-				}
-			}
-		default:
-			panic("unknown character")
-		}
-	}
-	if spring_len > 0 {
-		live_counts = append(live_counts, spring_len)
-	}
-	//fmt.Println(counts, live_counts)
-	return slices.Equal(counts, live_counts)
+type state struct {
+	record_index int
+	count_index  int
+	cur_len      int
 }
 
-func compute_possibilities(record []rune, counts []int) int {
-	//fmt.Println(string(record))
-	if !validate_record(record, counts) {
-		return 0
+var memo map[state]int
+
+func compute_possibilities(s state, record []rune, counts []int) int {
+	// Look up result if present
+	if val, ok := memo[s]; ok {
+		return val
 	}
-	index := slices.Index(record, '?')
-	if index == -1 {
-		fmt.Println(string(record), "passes!")
-		return 1
+	// Reached the end
+	if s.record_index == len(record) {
+		if s.count_index == len(counts)-1 && s.cur_len == counts[len(counts)-1] {
+			return 1
+		} else if s.count_index == len(counts) && s.cur_len == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	ans := 0
+	orig_s := s
+	s.record_index++
+	if record[orig_s.record_index] == '#' {
+		s.cur_len++
+		ans += compute_possibilities(s, record, counts)
+	} else if record[orig_s.record_index] == '.' && s.cur_len == 0 {
+		ans += compute_possibilities(s, record, counts)
+	} else if record[orig_s.record_index] == '.' && s.count_index < len(counts) && s.cur_len == counts[s.count_index] {
+		s.count_index++
+		s.cur_len = 0
+		ans += compute_possibilities(s, record, counts)
+	} else if record[orig_s.record_index] == '?' {
+		// Add possibilities if '?' is '#' or '.'
+		// '#' case
+		ans += compute_possibilities(state{record_index: s.record_index, count_index: s.count_index, cur_len: s.cur_len + 1},
+			record, counts)
+		// '.' case
+		if s.cur_len == 0 {
+			ans += compute_possibilities(s, record, counts)
+		} else if s.count_index < len(counts) && s.cur_len == counts[s.count_index] {
+			ans += compute_possibilities(state{record_index: s.record_index, count_index: s.count_index + 1, cur_len: 0},
+				record, counts)
+		}
 	}
 
-	fixed_copy := make([]rune, len(record))
-	broken_copy := make([]rune, len(record))
-
-	copy(fixed_copy, record)
-	copy(broken_copy, record)
-
-	fixed_copy[index] = '.'
-	broken_copy[index] = '#'
-
-	return compute_possibilities(fixed_copy, counts) + compute_possibilities(broken_copy, counts)
+	memo[orig_s] = ans
+	return ans
 }
 
 func parse_line(line string) int {
@@ -81,18 +69,28 @@ func parse_line(line string) int {
 		val, _ := strconv.Atoi(springs_str[i])
 		springs = append(springs, val)
 	}
-	fmt.Println(line)
 	var record []rune = []rune(tokens[0])
 
-	return compute_possibilities(record, springs)
+	records5x := make([]rune, 0, len(record)*5+4)
+	springs5x := make([]int, 0, len(springs))
+
+	for i := 0; i < 5; i++ {
+		springs5x = append(springs5x, springs...)
+		if i > 0 {
+			records5x = append(records5x, '?')
+		}
+		records5x = append(records5x, record...)
+	}
+
+	return compute_possibilities(state{0, 0, 0}, records5x, springs5x)
 }
 
 func main() {
 	lines := utils.Open_file("puzzle_input.txt")
 	sum := 0
 	for _, line := range lines {
+		memo = make(map[state]int)
 		val := parse_line(line)
-		//fmt.Println(val)
 		sum += val
 	}
 	fmt.Println(sum)
